e4bf8a03d156ad58dcbe925fd204d8a221394244
rob <roblourens@gmail.com>
2011-07-21 21:44:10 -0500

    Why did I have this crap in the repo? And why the .svn? Fail

--- testOrOld/coordTest.py
+++
@@ -1,8 +0,0 @@
-import cv
-
-image = cv.LoadImage('/home/rob/facealign/in/01.JPG', 1)
-cv.Rectangle(image, (0,0), (20,20), (255,255,255))
-win = 'test'
-cv.NamedWindow(win, 1)
-cv.ShowImage(win, image)
-cv.SaveImage('/home/rob/facealign/out.jpg', image)
--- testOrOld/negativeROI.py
+++
@@ -1,8 +0,0 @@
-import cv
-
-img = cv.LoadImage('../../dat/raw/DSC05172.JPG', 1)
-cv.SetImageROI(img, (1000, 1000, 1000, 1000))
-newImg = cv.CreateImage((1000, 1000), cv.IPL_DEPTH_8U, 3)
-cv.CopyMakeBorder(img, newImg, (0, 0), 0)
-
-cv.SaveImage('test.jpg', newImg)
